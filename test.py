6a1a646d156354ce77ee6a62718a18113f96511a
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-06-10 19:18:08 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import redis
 import os
 
+print os.environ.get('WERCKER_REDIS_HOST'
+
 r = redis.StrictRedis(host=os.getenv("WERCKER_REDIS_HOST"), port=6379, db=0)
 r.set('foo', 'bar')
 result = r.get('foo')
